096e61f1f0ceb2a7f53940968986bcc400341a5f
Per Rutquist <per.rutquist@gmail.com>
2022-06-23 12:15:24 +0200

    Benchmark with Distributed

--- test/runbenchmarks.jl
+++ test/runbenchmarks.jl
@@ -1,4 +1,9 @@
-using TriangularIndices
+using Distributed
+addprocs(3)
+
+@everywhere push!(LOAD_PATH, splitdir(@__DIR__)[1])
+@everywhere using TriangularIndices
+
 using FLoops
 using BenchmarkTools
 
@@ -22,18 +27,26 @@ end
 
 function s3(n)
     s = 0.0
-    @floop for (i,j) = UpperTriangularIndices(n)
+    FLoops.@floop for (i,j) = UpperTriangularIndices(n)
         @reduce s += i/j
     end
     s
 end
 
+function s4(n)
+    @distributed (+) for (i,j) = UpperTriangularIndices(n)
+        i/j
+    end
+end
+
 @assert s1(1000) ≈ s2(1000) 
 @assert s2(1000) ≈ s3(1000)
+@assert s3(1000) ≈ s4(1000)
 
 @btime s1(1000)
 @btime s2(1000)
 @btime s3(1000)
+@btime s4(1000)
 
 function t1!(A)
     @assert size(A,1) == size(A,2)
